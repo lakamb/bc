baf3db53ee29925dd3d2e0aaf48470d3e4bbd10f
lakamb <lakamb@bk.ru>
2019-02-07 16:45:32 +0300

    Delete hex.py

--- hex.py
+++
@@ -1,8 +0,0 @@
-#хэш
-
-import hashlib
-
-file = open('C:/Users/sales5/Desktop/testfile.txt', 'rb').read() #читаем файл (как бинарные данные)
-
-h = hashlib.md5(file).hexdigest() #присваиваем переменной h хэш читаемого файла
-print(h) #выводим хэш
