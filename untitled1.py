0fac6bb706366542055af80707a7f490d7314956
lakamb <lakamb@bk.ru>
2019-02-07 16:45:53 +0300

    Delete untitled1.py

--- untitled1.py
+++
@@ -1,9 +0,0 @@
-#bc
-#genezis block. первый блок. у него нет значения хэша предыдущего блока
-#JSON объект
-{
-     "name": "Oleg",
-     "amount": 10,
-     "to_whom": "Petya Petrov"
-     "hash": "" #хэш предыдущего
- }
